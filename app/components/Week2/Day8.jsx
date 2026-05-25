3ca2b004afe09b6131f7575ec621e5be6880a749
Gaurav Verma <briskgaurav@gmail.com>
2026-05-25 09:39:43 +0530

    DAY 8

---
+++ app/components/Week2/Day8.jsx
@@ -0,0 +1,32 @@
+"use client";
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { color, sin, step, time, uv } from "three/tsl";
+import { MeshBasicNodeMaterial, WebGPURenderer } from "three/webgpu";
+
+export default function Day8() {
+    const stepValue = 0.95
+  const material = new MeshBasicNodeMaterial();
+  const tilesUv = uv().mul(5).fract();
+  const lineX = step(stepValue, tilesUv.x);
+  const LineBottom = step(stepValue, tilesUv.y.oneMinus(1));
+  const lineLeft = step(stepValue, tilesUv.x.oneMinus(1));
+  const lineY = step(stepValue, tilesUv.y);
+  const grid = lineX.add(lineY).add(lineLeft).add(LineBottom);
+  material.colorNode = color(grid);
+
+  return (
+    <Canvas
+      gl={async (props) => {
+        const renderer = new WebGPURenderer(props);
+        await renderer.init();
+        return renderer;
+      }}
+    >
+      <mesh material={material}>
+        <planeGeometry args={[2, 2, 10, 10]} />
+      </mesh>
+      {/* <OrbitControls /> */}
+    </Canvas>
+  );
+}
--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,16 @@
-'use client'
-import Day1 from './components/Day1'
-import Day2 from './components/Day2'
-import Day3 from './components/Day3'
-import Day4 from './components/Day4'
-import Day5 from './components/Day5'
-import Miniproject1 from './components/MiniProject1'
+"use client";
+import Day1 from "./components/Week1/Day1";
+import Day2 from "./components/Week1/Day2";
+import Day3 from "./components/Week1/Day3";
+import Day4 from "./components/Week1/Day4";
+import Day5 from "./components/Week1/Day5";
+import Miniproject1 from "./components/Week1/MiniProject1";
+import Day8 from '@/app/components/Week2/Day8'
 
 export default function page() {
   return (
-    <div className='h-screen w-full relative'>
-      <Miniproject1 />
+    <div className="h-screen w-full relative">
+       <Day8 />
     </div>
-  )
+  );
 }
