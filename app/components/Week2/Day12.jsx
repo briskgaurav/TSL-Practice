ea30b53ffc2be4c8fd6ed2a4fed95a37c4a3ee04
Gaurav Verma <briskgaurav@gmail.com>
2026-06-03 01:25:44 +0530

    SHADER ENHANCEMENT OF DAY 12

--- app/components/Week2/Day12.jsx
+++ app/components/Week2/Day12.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { Canvas } from "@react-three/fiber";
-import { color, sin, time, uv } from "three/tsl";
+import { mix, sin, time, uv, vec3 } from "three/tsl";
 import { MeshBasicNodeMaterial, WebGPURenderer } from "three/webgpu";
 
 
@@ -9,13 +9,18 @@ export default function Day12() {
 
     const FREQUENCY = 5
     const WAVE_AMPLITUDE = 2
+    const WAVE2_AMPLITUDE = 4
     const TIME_MULTIPLIER = 2
     const material = new MeshBasicNodeMaterial();
-    material.colorNode = color(sin(uv().y.mul(FREQUENCY).add(sin(uv().x.mul(WAVE_AMPLITUDE).add(time).mul(TIME_MULTIPLIER)))).add(.5).mul(.5), 0, 0)
+    const wave1 = sin(uv().x.mul(WAVE_AMPLITUDE).add(time).mul(TIME_MULTIPLIER))
+    const wave2 = sin(uv().x.mul(WAVE2_AMPLITUDE).add(time).mul(TIME_MULTIPLIER))
+    const wave = wave1.add(wave2)
+    const finalShader = sin(uv().y.mul(FREQUENCY).add(wave)).mul(.5).add(.2)
+    material.colorNode = mix( vec3(0, 0, 0),vec3(1, 0, 0), finalShader)
 
     return (
         <>
-        <h1 className="absolute top-[4vw] left-1/2 -translate-x-1/2 text-center text-red-700 w-full  z-999 text-[10vw] font-bold font-sans">SHADER WAVE</h1>
+            <h1 className="absolute top-[4vw] select-none left-1/2 -translate-x-1/2 text-center text-red-700 w-full  z-999 text-[10vw] font-bold font-sans">SHADER WAVE</h1>
             <Canvas
                 gl={async (props) => {
                     const renderer = new WebGPURenderer(props);
